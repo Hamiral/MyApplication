dd40e0a5e7a8039e39c8dce27f70aeb1d3ae4620
LorAlex <lor.alex.f@gmail.com>
2015-03-23 15:57:35 +0100

    CorrectionLayout

---
+++ app/src/main/java/com/example/pi2013/myapplication/LostAccountActivity.java
@@ -0,0 +1,18 @@
+package com.example.pi2013.myapplication;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class LostAccountActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lost_account);
+    }
+
+
+}
