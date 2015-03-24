adc41463666f9e6932588186aeafbdc33968477d
LorAlex <lor.alex.f@gmail.com>
2015-03-24 16:51:52 +0100

    StringsValues and ChangeLanguage

--- app/src/main/java/com/example/pi2013/myapplication/DisplayMessageActivity.java
+++ app/src/main/java/com/example/pi2013/myapplication/DisplayMessageActivity.java
@@ -1,10 +1,13 @@
 package com.example.pi2013.myapplication;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.DisplayMetrics;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 
 public class DisplayMessageActivity extends BaseActivity {
@@ -20,5 +23,14 @@ public class DisplayMessageActivity extends BaseActivity {
         textView.setText(message);
         setContentView(textView);
     }
-
+    public void setLocale(String lang) {
+        Locale myLocale = new Locale(lang);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = myLocale;
+        res.updateConfiguration(conf, dm);
+        Intent refresh = new Intent(this, DisplayMessageActivity.class);
+        startActivity(refresh);
+    }
 }
