93859f9f56da2d1c28d02ade973b5216ac3d1852
LorAlex <lor.alex.f@gmail.com>
2015-03-27 15:37:43 +0100

    Merge 27/03/15

--- app/src/main/java/com/example/pi2013/myapplication/MyActivity.java
+++ app/src/main/java/com/example/pi2013/myapplication/MyActivity.java
@@ -2,11 +2,13 @@ package com.example.pi2013.myapplication;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MyActivity extends BaseActivity{
@@ -44,16 +46,23 @@ public class MyActivity extends BaseActivity{
     {
         GlobalVariable appState = ((GlobalVariable)getApplicationContext());
         Button button_dynamic = (Button) findViewById(R.id.button_dynamic);
+        TextView textView=(TextView) findViewById(R.id.main_state);
         if(appState.getLogged() && appState.getHotspot()) {
             button_dynamic.setText("Start Browsing");
+            textView.setText("Hotspot found !");
+            textView.setTextColor(Color.GREEN);
         }
         else if (appState.getLogged() && !appState.getHotspot())
         {
             button_dynamic.setText("Find a Hotspot");
+            textView.setText("No Hotspot found.");
+            textView.setTextColor(Color.RED);
         }
         else
         {
             button_dynamic.setText("Sign In");
+            textView.setText("No Hotspot found.");
+            textView.setTextColor(Color.RED);
         }
 
     }
